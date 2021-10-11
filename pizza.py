f804ae98d51e134baecf62a432da0bf61e383fee
vitvara <gridxlgm@gmail.com>
2021-10-11 11:33:57 +0700

    refactoring 1

--- pizza.py
+++ pizza.py
@@ -1,24 +1,35 @@
+from enum import Enum
+from typing import Type
+
+class PizzaSize(Enum):
+    small = 120
+    medium = 200
+    large = 280
+
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
-    def __init__(self, size):
+    def __init__(self, size: PizzaSize):
+        if not isinstance(size , PizzaSize):
+            raise TypeError("size must be a PizzaSize")
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
+        price = self.size.value + 20*len(self.toppings)
         return price
     
-    def addTopping(self, topping):
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
 
+    def __str__(self):
+        description = "A " + self.size.name
+        if self.toppings:
+            description += " pizza with "+ ", ".join(self.toppings)
+        else:
+            description += " plain cheeze pizza"
+        return description
--- pizzashop.py
+++ pizzashop.py
@@ -1,39 +1,37 @@
-from pizza import Pizza
+from pizza import Pizza, PizzaSize
+import pizza as pz
 
 # This function shows a limitation on tool-assisted
 # refactoring in a dynamic language like Python.
 #
-# When you rename the Pizza getPrice method to get_price,
+# When you rename the Pizza get_price method to get_price,
 # does it rename the method here?
 # - if no type annotation on the pizza parameter, maybe not
 # - if use type annotation ':Pizza' on the parameter, it should
 
-def order_pizza(pizza):
+
+def order_pizza(pizza: Pizza):
     """Print a description of a pizza, along with its price."""
 
     # create printable description of the pizza such as
     # "small pizza with muschroom" or "small plain pizza"
-    description = pizza.size
-    if pizza.toppings:
-        description += " pizza with "+ ", ".join(pizza.toppings)
-    else:
-        description += " plain cheeze pizza"
-    print(f"A {description}")
-    print("Price:", pizza.getPrice())
+    # print "A small pizza with seafood, mushroom"
+    print(pizza)
+    print("Price:", pizza.get_price())
 
 
 if __name__ == "__main__":
-    pizza = Pizza('small')
-    pizza.addTopping("mushroom")
-    pizza.addTopping("tomato")
-    pizza.addTopping("pinapple")
+    pizza = Pizza(PizzaSize.small)
+    pizza.add_topping("mushroom")
+    pizza.add_topping("tomato")
+    pizza.add_topping("pinapple")
     order_pizza(pizza)
 
     # a plain pizza
-    pizza2 = Pizza("medium")
+    pizza2 = Pizza(PizzaSize.medium)
     order_pizza(pizza2)
 
     # pizza with only one topping
-    pizza3 = Pizza("large")
-    pizza3.addTopping("seafood")
+    pizza3 = Pizza(PizzaSize.large)
+    pizza3.add_topping("seafood")
     order_pizza(pizza3)
